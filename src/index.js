b2327cf071965a088721c1cb95c4b4d698244edb
den <you@example.com>
2024-08-06 22:01:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let num = ''
+  let str_n = String(Math.abs(n))
+  let cnt_n = str_n.length - 1
+  while (cnt_n >= 0) {
+    num += str_n[cnt_n]
+    cnt_n--
+  }
+  return Number(num);
 }
