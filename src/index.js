48f87eda5a13cf55dc2fa6059f9bedac19118629
den <you@example.com>
2025-08-28 12:50:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = n.toString();
+  let res = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    res += str[i];
+  }
+  return res.replace('-', '');
 };
